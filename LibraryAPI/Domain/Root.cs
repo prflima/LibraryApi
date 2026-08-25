2d80784cdb2c6709f444c0aab1906c6fa9f4d3f8
Paulo Ricardo <paulo.tolkien@outlook.com>
2026-08-24 23:27:54 -0300

    Building domain models

---
+++ LibraryAPI/Domain/Root.cs
@@ -0,0 +1,8 @@
+﻿namespace LibraryAPI.Domain
+{
+    public abstract class Root 
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ LibraryAPI/Domain/User.cs
@@ -0,0 +1,20 @@
+﻿using LibraryAPI.Enums;
+
+namespace LibraryAPI.Domain
+{
+    public class User : Root
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public UserType UserType { get; private set; }
+
+        public User(string name, string email, UserType userType)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Email = email;
+            UserType = userType;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
